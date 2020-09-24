8faefd00c77ec6e0f56321a4952253ed7c844bcf
Henry Addison <henryaddison@users.noreply.github.com>
2020-09-24 13:02:22 +0100

    add database and migrations system
    with an Example class to get started

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ Basic webapp for gathering adversial examples
 ### Dependencies
 
 * Python 3
-* Eventually a database (probably PostgreSQL or SQLite)
+* Eventually a database (probably PostgreSQL)
 
 Assuming *nix like OS (e.g. Mac or Linux), can add Windows instructions if needed.
 
+0. Clone this repo
 1. Recommend using a virtual environment venv - `python3 -m venv venv` as a one-off to create then `. venv/bin/activate` to activate each time you work on project
 2. `pip install -r requirements.txt`
+3. Create a database
+4. Create your own `.env` file (base it off `.env.example` but you'll need to adapt it to your own machine)
+5. Run any database migrations: `FLASK_APP=adverserial FLAST_ENV=development flask db upgrade`
 
 ## Usage
 
@@ -20,3 +24,7 @@ Assuming *nix like OS (e.g. Mac or Linux), can add Windows instructions if neede
 `FLASK_APP=adverserial FLAST_ENV=development flask run`
 
 App will be available at http://localhost:5000
+
+### Run database migrations
+
+`FLASK_APP=adverserial FLAST_ENV=development flask db upgrade`
--- adverserial/__init__.py
+++ adverserial/__init__.py
@@ -1,13 +1,23 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__, instance_relative_config=True)
   app.config.from_mapping(
     SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'adversial.sqlite')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
   )
 
   if test_config is None:
@@ -20,8 +30,20 @@ def create_app(test_config=None):
   except OSError:
     pass
 
+  db.init_app(app)
+  migrate.init_app(app, db)
+
   @app.route('/hello')
   def hello():
     return 'Hello, World!'
 
   return app
+
+class Example(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  context = db.Column(db.String)
+  question = db.Column(db.String)
+  answer_a = db.Column(db.String)
+  answer_b = db.Column(db.String)
+  answer_c = db.Column(db.String)
+  correct_answer = db.Column(db.String)
