3d28331db44c8581dcb86320f53616953620e51d
Henry Addison <henryaddison@users.noreply.github.com>
2020-09-28 16:19:22 +0100

    use DATABASE_URL to configure access to db

--- adverserial/__init__.py
+++ adverserial/__init__.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
   app = Flask(__name__, instance_relative_config=True)
   app.config.from_mapping(
     SECRET_KEY='dev',
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
   )
 
   if test_config is None:
