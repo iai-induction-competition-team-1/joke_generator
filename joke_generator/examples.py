0bcc2ae8df67aa6d7a4299e0d1460538ac1b3c11
Henry Addison <henryaddison@users.noreply.github.com>
2020-10-01 14:55:54 +0100

    cli command to export new examples based on jokes provided

--- joke_generator/examples.py
+++ joke_generator/examples.py
@@ -2,12 +2,13 @@ import ast
 import click
 import functools
 import json
+import random
 
 from flask import (
   Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-from joke_generator import db, SeedExample
+from joke_generator import db, SeedExample, NewAnswerExample
 
 bp = Blueprint('examples', __name__, url_prefix='/examples')
 
@@ -49,3 +50,34 @@ def replace(input_file):
         )
         db.session.add(new_example)
     db.session.commit()
+
+@bp.cli.command('export')
+def export():
+    for new_example in NewAnswerExample.query.all():
+      new_answer = new_example.answer
+
+      correct_answer = new_example.seed_example.correct_answer()
+
+      other_seed_example_answers = ['A', 'B', 'C']
+      other_seed_example_answers.remove(new_example.seed_example.correct_answer_index)
+      another_answer_index = random.choice(other_seed_example_answers)
+      if another_answer_index.lower() == 'a':
+        another_answer = new_example.seed_example.answer_a
+      elif another_answer_index.lower() == 'b':
+        another_answer = new_example.seed_example.answer_b
+      elif another_answer_index.lower() == 'c':
+        another_answer = new_example.seed_example.answer_c
+
+      answer_list = [new_answer, correct_answer, another_answer]
+      random.shuffle(answer_list)
+      correct_answer_index = answer_list.index(correct_answer)
+      correct_answer_letter = ['A', 'B', 'C'][correct_answer_index] # conver 0,1,2 to A,B,C
+
+      new_example_data = {'context': new_example.seed_example.context,
+        'question': new_example.seed_example.question,
+        'answerA': answer_list[0],
+        'answerB': answer_list[1],
+        'answerC': answer_list[2],
+        'correct': correct_answer_letter
+      }
+      print(json.dumps(new_example_data))
